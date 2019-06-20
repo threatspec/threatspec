22175b08834d22291dab80e50f15466224a2b94f
Fraser Scott <fraser.scott@gmail.com>
2019-06-20 08:42:57 +0100

    adding supported python versions

--- setup.py
+++ setup.py
@@ -34,6 +34,8 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Security',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7'
     ]
 )
